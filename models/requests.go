1702d2c0a4612d05095b2f45484461f99f6f1fab
lucasnevespereira <pereiraneveslucas@gmail.com>
2023-08-25 21:47:03 +0100

    update invite request

--- models/requests.go
+++ models/requests.go
@@ -1,9 +1,8 @@
 package models
 
 type SendInviteRequest struct {
-	Username   string `json:"username"`
-	Email      string `json:"email"`
-	Token      string `json:"token"`
-	FamilyName string `json:"familyName"`
-	FamilyID   string `json:"familyId"`
+	Username string `json:"username"`
+	Email    string `json:"email"`
+	Token    string `json:"token"`
+	Slug     string `json:"slug"`
 }
