b08d9599efd4c312f42a1e1471062ed17655d6b9
lucasnevespereira <pereiraneveslucas@gmail.com>
2023-08-25 22:43:46 +0100

    add user id to invitation redirection

--- models/requests.go
+++ models/requests.go
@@ -1,6 +1,7 @@
 package models
 
 type SendInviteRequest struct {
+	UserID   string `json:"userID"`
 	Username string `json:"username"`
 	Email    string `json:"email"`
 	Token    string `json:"token"`
